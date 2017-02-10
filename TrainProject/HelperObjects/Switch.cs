ce56d866484b5cce0ba35cbee1163cd5b659d40c
Gabe Wells <Gabe Wells>
2017-02-10 16:06:12 -0500

    Add switch class, move switch and block to HelperObject folder

---
+++ TrainProject/HelperObjects/Switch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainProject
+{
+    class Switch
+    {
+        int switchId { get; set; }
+        int sourceBlockId { get; set; }
+        int targetBlockId1 { get; set; }
+        int targetBlockId2 { get; set; }
+        int currentState { get; set; }
+
+        public Switch(int s, int sb, int t1, int t2)
+        {
+            switchId = s;
+            sourceBlockId = sb;
+            targetBlockId1 = t1;
+            targetBlockId2 = t2;
+            currentState = targetBlockId1;
+        }
+    }
+}
