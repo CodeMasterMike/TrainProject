8a2e5502c82f09d8cb71ba41913e015ed9449ccd
Gabe Wells <Gabe Wells>
2017-02-10 15:00:02 -0500

    Begin implementing Track Controller -created block class -created test class

--- TrainProject/Homepage.cs
+++ TrainProject/Homepage.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Track_Layout_UI;
 using TrackController;
 using CTC;
+using TrainProject;
 
 namespace Train_Project
 {
@@ -30,6 +31,8 @@ namespace Train_Project
         {
             TrackControllerWindow trackControllerWindow = new TrackControllerWindow();
             trackControllerWindow.Show();
+            TrackControllerTest test = new TrackControllerTest();
+            test.run();
         }
 
         private void openCTC_Click(object sender, EventArgs e)
