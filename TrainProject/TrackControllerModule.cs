243c0c274bb59c5472ee14d0834678489bb7857e
Kaitlyn <kes156@pitt.edu>
2017-02-15 12:59:16 -0500

    merged in some new files

---
+++ TrainProject/TrackControllerModule.cs
@@ -0,0 +1,22 @@
+﻿//This is the track controller module that actually controls the track
+//This class is the main point of communication with the system. 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackController
+{
+    public class TrackController
+    {
+        private int controllerId;
+        //need to declare an array of blocks that this controller will keep track of
+        //maybe an array of switches too
+        public void testMethod()
+        {
+            Console.Write("testmethod\n");
+        }
+
+    }
+}
---
+++ TrainProject/TrackControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainProject
+{
+    class TrackControllerTest
+    {
+        public void run()
+        {
+            TrackController.TrackController tc = new TrackController.TrackController();
+            tc.testMethod();
+        }
+    }
+}
