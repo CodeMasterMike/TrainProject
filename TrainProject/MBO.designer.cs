cf54ca955093cd449b405c9d3032d9e313d03d0b
Alejandro Porras <ajp93@pitt.edu>
2017-04-19 11:28:05 -0400

    fixing bad pull and gui changes

--- TrainProject/MBO.designer.cs
+++ TrainProject/MBO.designer.cs
@@ -74,7 +74,7 @@
             this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(779, 408);
+            this.tabControl1.Size = new System.Drawing.Size(779, 428);
             this.tabControl1.TabIndex = 1;
             // 
             // tabPage1
@@ -85,7 +85,7 @@
             this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
-            this.tabPage1.Size = new System.Drawing.Size(771, 382);
+            this.tabPage1.Size = new System.Drawing.Size(771, 402);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Scheduling";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -138,16 +138,16 @@
             this.panel1.Controls.Add(this.button2);
             this.panel1.Location = new System.Drawing.Point(107, 14);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(659, 363);
+            this.panel1.Size = new System.Drawing.Size(661, 388);
             this.panel1.TabIndex = 4;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(509, 203);
+            this.button10.Location = new System.Drawing.Point(498, 253);
             this.button10.Margin = new System.Windows.Forms.Padding(2);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(129, 29);
+            this.button10.Size = new System.Drawing.Size(139, 39);
             this.button10.TabIndex = 16;
             this.button10.Text = "Passenger Movement";
             this.button10.UseVisualStyleBackColor = true;
@@ -155,10 +155,10 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(343, 203);
+            this.button7.Location = new System.Drawing.Point(332, 253);
             this.button7.Margin = new System.Windows.Forms.Padding(2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(129, 29);
+            this.button7.Size = new System.Drawing.Size(140, 39);
             this.button7.TabIndex = 15;
             this.button7.Text = "View Driver Schedule";
             this.button7.UseVisualStyleBackColor = true;
@@ -166,10 +166,10 @@
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(510, 107);
+            this.button9.Location = new System.Drawing.Point(498, 124);
             this.button9.Margin = new System.Windows.Forms.Padding(2);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(128, 28);
+            this.button9.Size = new System.Drawing.Size(140, 39);
             this.button9.TabIndex = 14;
             this.button9.Text = "Send Authority";
             this.button9.UseVisualStyleBackColor = true;
@@ -177,10 +177,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(343, 156);
+            this.button1.Location = new System.Drawing.Point(332, 186);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 28);
+            this.button1.Size = new System.Drawing.Size(140, 40);
             this.button1.TabIndex = 13;
             this.button1.Text = "Create Driver Schedule\r\n";
             this.button1.UseVisualStyleBackColor = true;
@@ -188,10 +188,10 @@
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(510, 253);
+            this.button8.Location = new System.Drawing.Point(498, 319);
             this.button8.Margin = new System.Windows.Forms.Padding(2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(128, 28);
+            this.button8.Size = new System.Drawing.Size(139, 38);
             this.button8.TabIndex = 12;
             this.button8.Text = "Fix Antenna";
             this.button8.UseVisualStyleBackColor = true;
@@ -199,10 +199,10 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(343, 107);
+            this.button6.Location = new System.Drawing.Point(332, 124);
             this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(129, 28);
+            this.button6.Size = new System.Drawing.Size(140, 39);
             this.button6.TabIndex = 11;
             this.button6.Text = "View Train Schedule";
             this.button6.UseVisualStyleBackColor = true;
@@ -210,10 +210,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(510, 156);
+            this.button5.Location = new System.Drawing.Point(498, 186);
             this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(128, 28);
+            this.button5.Size = new System.Drawing.Size(138, 40);
             this.button5.TabIndex = 10;
             this.button5.Text = "Calculate Variance";
             this.button5.UseVisualStyleBackColor = true;
@@ -222,19 +222,19 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::TrainProject.Properties.Resources.Track_Layout;
-            this.pictureBox1.Location = new System.Drawing.Point(6, -33);
+            this.pictureBox1.Location = new System.Drawing.Point(6, -10);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(298, 405);
+            this.pictureBox1.Size = new System.Drawing.Size(298, 417);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(343, 252);
+            this.button4.Location = new System.Drawing.Point(332, 319);
             this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(129, 29);
+            this.button4.Size = new System.Drawing.Size(140, 38);
             this.button4.TabIndex = 8;
             this.button4.Text = "Break Antenna";
             this.button4.UseVisualStyleBackColor = true;
@@ -242,10 +242,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(510, 57);
+            this.button3.Location = new System.Drawing.Point(498, 57);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(128, 28);
+            this.button3.Size = new System.Drawing.Size(140, 40);
             this.button3.TabIndex = 7;
             this.button3.Text = "Get Position/Speed";
             this.button3.UseVisualStyleBackColor = true;
@@ -253,10 +253,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(343, 57);
+            this.button2.Location = new System.Drawing.Point(332, 57);
             this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(129, 28);
+            this.button2.Size = new System.Drawing.Size(140, 40);
             this.button2.TabIndex = 6;
             this.button2.Text = "Create Train Schedule";
             this.button2.UseVisualStyleBackColor = true;
@@ -286,7 +286,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 443);
+            this.label1.Location = new System.Drawing.Point(6, 478);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(160, 20);
@@ -299,7 +299,7 @@
             this.label15.AutoSize = true;
             this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label15.ForeColor = System.Drawing.Color.Lime;
-            this.label15.Location = new System.Drawing.Point(170, 443);
+            this.label15.Location = new System.Drawing.Point(170, 479);
             this.label15.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(96, 20);
@@ -310,7 +310,7 @@
             // 
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label17.Location = new System.Drawing.Point(287, 443);
+            this.label17.Location = new System.Drawing.Point(287, 479);
             this.label17.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(129, 20);
@@ -322,7 +322,7 @@
             this.label18.AutoSize = true;
             this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label18.ForeColor = System.Drawing.Color.Lime;
-            this.label18.Location = new System.Drawing.Point(420, 443);
+            this.label18.Location = new System.Drawing.Point(420, 478);
             this.label18.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(58, 20);
@@ -334,7 +334,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(508, 443);
+            this.label3.Location = new System.Drawing.Point(508, 478);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(76, 20);
@@ -347,7 +347,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Lime;
-            this.label4.Location = new System.Drawing.Point(588, 443);
+            this.label4.Location = new System.Drawing.Point(588, 478);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(15, 20);
@@ -359,7 +359,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(789, 472);
+            this.ClientSize = new System.Drawing.Size(802, 508);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label18);
