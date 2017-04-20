e382ce4f9b0cd9e3122dcbf8fedfdabf6ced5cb6
Naran <nab115@pitt.edu>
2017-04-20 02:56:55 -0400

    Finished commenting code and made it so you can enter kp and ki if you want, if not they have default values

--- TrainProject/HelperObjects/PowerController.cs
+++ TrainProject/HelperObjects/PowerController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//PowerController helper class
+//Created by : Naran Babha
+//Date Created : 2/20/2017
+//This class calculates the power based on a current speed and set speed, and past errors in speed
+//it is hard coded with kp = 40000 and ki = 200
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +28,7 @@ namespace TrainProject
         double power2 = 0;
         double power3 = 0;
         Random rnd;
-
+        //constructor
         public PowerController(double m, double mP)
         {
             maxPower = mP;
@@ -31,10 +37,14 @@ namespace TrainProject
 
         public double getPower(double cS, double sS)
         {
+            //randomly get a number 0-2 to choose outcome from power calculation
             int choice = rnd.Next(3);
+            //claculate the power three times for triple redundancy
+            //this ensures vital operation of the power calculation
             power1 = getPower1(cS, sS);
             power2 = getPower2(cS, sS);
             power3 = getPower3(cS, sS);
+           
             if (power1 == power2 && power2 == power3)
             {
                 if (choice == 0)
@@ -102,6 +112,14 @@ namespace TrainProject
             else uk = u;
             if (power < 0) power = 0;
             return power;
+        }
+        public void setKP(double K)
+        {
+            Kp = K;
+        }
+        public void setKI(double K)
+        {
+            Ki = K;
         }
     }
 }
